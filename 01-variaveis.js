b51a86f307077aa3fd6acd315156cf2e1eb6109a
italo.francesco727@gmail.com <italo.francesco727@gmail.com>
2024-02-07 11:26:06 -0300

    javascript- basico

---
+++ 01-variaveis.js
@@ -0,0 +1,10 @@
+//Criando variáveis com "var" (var nome variaável= <valor>;)
+var nome = "Ítalo";
+var idade = 16;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos");
+console.log("Agora fiz aniversário! 🥳 Tenho" , idade +1, "anos!");
+
+//aumentando automagicamente +1 (autoincremento)
+idade = idade ++ //idade = idade +1;
+console.log("Agora fiz aniversário! 🥳 Tenho" , idade , "anos");
---
+++ 02-variaveis.js
@@ -0,0 +1,25 @@
+//utilizando operadores aritiméticos com JavaScript
+
+console.log("A soma entre 8 e 7 é:",8+7)
+
+var x = 14
+var y = 9
+console.log("A soma entre x e y é:",x+y)
+
+var soma = x + y;
+var subtracao = x - y;
+var multiplicacao = x * y;
+var divisao = x / y; 
+
+console.log("Soma = " , soma);
+console.log("Subtracao = " , subtracao);
+console.log("Multiplicacao = " , multiplicacao);
+console.log("Divisao= " , divisao);
+
+// Regras para nomear identificadores  (var , const ou let)
+// Podem começar com A-Z, a-z, _ ou $
+
+var MinhaVariavel = 10;
+var  outraVariavel = 20;
+const _minhaConstante = 3.1514;
+var $minhaVariavel = 11;
