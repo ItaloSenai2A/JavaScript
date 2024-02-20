4b366c39ec428d9e3eb4d8b5c38d49d34e7c5a91
italo.francesco727@gmail.com <italo.francesco727@gmail.com>
2024-02-20 14:17:41 -0300

    Entrega da atividade JavaScript

---
+++ 06-variaveisLogicas.js
@@ -0,0 +1,13 @@
+//Variáveis lógicas (boolean)
+
+var nome = "Ítalo";
+var rico = false;
+var eProfessor = true;
+
+console.log("Nome:",nome,"| É professor?", eProfessor, "| É rico?", rico);
+console.log("------------------------------------------------")
+
+var nome = "Ítalo"
+var jogador = false
+
+console.log("Olá meu nome é " +nome+" | É jogador? " + jogador);
---
+++ Atividade02-Ítalo.js
@@ -0,0 +1,45 @@
+var nome = "Ítalo";
+var nasceu = 2008;
+var texto1 = "nasceu em";
+var ano = 2024;
+var texto2 = "tem";
+var texto3 = "anos";
+
+console.log(nome, "" + texto1, "" + nasceu);
+console.log(nome, "" + texto2, "" + ano - nasceu, "" + texto3);
+console.log("----------------------------");
+
+var comprimento = 12;
+var largura = 6;
+var texto4 = "A área do quadrado é";
+var volume = 5;
+var texto5 = "O valor da dimensão é";
+var area = 72;
+
+console.log(texto4, "" + comprimento * largura, "cm");
+console.log(texto5, "" + area * volume, "cm");
+console.log("-----------------------------");
+
+var a = 2;
+var b = 3;
+var c = -1;
+
+
+var delta = b**2 - 4 * a * c;
+console.log("O valor de delta é", delta);
+console.log("-----------------------------");
+
+var compra = 450
+var desc = 3 
+var divisao = 100
+console.log(450 / 100 * 3);
+console.log(450 - 13,5)
+console.log("------------------------------");
+ 
+console.log(95 / 3.5);
+
+
+
+
+
+
