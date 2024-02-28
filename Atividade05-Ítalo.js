82faa672487d7e9e1ccbf323ccc5931f81d75ee3
italo.francesco727@gmail.com <italo.francesco727@gmail.com>
2024-02-28 11:04:01 -0300

    Atividade sobre funções

---
+++ Atividade05-Ítalo.js
@@ -0,0 +1,14 @@
+function idade (ano) {
+    var year = 2024
+    return year - ano
+}
+console.log(`A sua idade é ${idade(2008)}`);
+console.log(`--------------------------------------`);
+
+//Exercício 2:
+
+function km (combustivel) {
+    var km = 12
+    return km * combustivel
+}
+console.log(`Tendo 5 litros, o carro viaja ${km(48)} km`);
